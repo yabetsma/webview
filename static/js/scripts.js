612281257ae76e68b59d06244566cc1067a57d9e
eyob2one <eyob2one@gmail.com>
2024-08-24 09:45:15 -0700

    update url

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
             event.preventDefault();
             const formData = new FormData(addChannelForm);
 
-            fetch('https://1e92-93-190-142-107.ngrok-free.app/add_channel', { // Replace with your ngrok URL
+            fetch('https://6d44-93-190-142-107.ngrok-free.app/add_channel', { // Replace with your ngrok URL
                 method: 'POST',
                 body: formData
             })
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
             event.preventDefault();
             const formData = new FormData(createGiveawayForm);
 
-            fetch('https://1e92-93-190-142-107.ngrok-free.app/create_giveaway', { // Replace with your ngrok URL
+            fetch('https://6d44-93-190-142-107.ngrok-free.app/create_giveaway', { // Replace with your ngrok URL
                 method: 'POST',
                 body: formData
             })
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
         announceWinnersButton.addEventListener('click', function() {
             const giveawayId = giveawayIdInput.value;
 
-            fetch(`https://1e92-93-190-142-107.ngrok-free.app/announce_winners/${giveawayId}`) // Replace with your ngrok URL
+            fetch(`https://6d44-93-190-142-107.ngrok-free.app/announce_winners/${giveawayId}`) // Replace with your ngrok URL
             .then(response => response.json())
             .then(data => {
                 if (data.success) {
