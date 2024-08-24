d35cf4727d71c86a769084e91afd318c6cac9e16
eyob2one <eyob2one@gmail.com>
2024-08-24 09:42:03 -0700

    update js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const addChannelForm = document.getElementById('addChannelForm');
+    const addChannelForm = document.getElementById('addChannelForm');  // Ensure this form ID matches your HTML
     const createGiveawayForm = document.getElementById('createGiveawayForm');
     const announceWinnersButton = document.getElementById('announceWinnersButton');
     const giveawayIdInput = document.getElementById('giveawayId');
@@ -14,18 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 method: 'POST',
                 body: formData
             })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
+            .then(response => response.json())
             .then(data => {
                 if (data.success) {
                     alert(data.message);
                     addChannelForm.reset(); // Clear the form
-                    // Optionally, reload or update the page to reflect changes
-                    location.reload(); // Uncomment if you want to refresh the page
                 } else {
                     alert('Failed to add channel: ' + data.message);
                 }
@@ -46,12 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 method: 'POST',
                 body: formData
             })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
+            .then(response => response.json())
             .then(data => {
                 if (data.success) {
                     alert(data.message);
@@ -72,12 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const giveawayId = giveawayIdInput.value;
 
             fetch(`https://1e92-93-190-142-107.ngrok-free.app/announce_winners/${giveawayId}`) // Replace with your ngrok URL
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
+            .then(response => response.json())
             .then(data => {
                 if (data.success) {
                     resultsDiv.innerHTML = `<h2>Winners</h2><ul>${data.winners.map(winner => `<li>${winner.username}</li>`).join('')}</ul>`;
